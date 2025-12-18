18f1182d289ecbd44b70a674d59008d6ec475684
Nay-VDp <malonn_775@icloud.com>
2025-12-18 21:18:57 +0100

    index.js

--- index.js
+++ index.js
@@ -4,38 +4,87 @@ document.body.innerHTML = `
 <head>
 <meta charset="UTF-8">
 <title>SQLI BY Imp0sant</title>
+
 <style>
-.link {
-    background-color: blue;
-    color: white;
-    display: inline-block;
-    padding: 12px 30px;
-    border-radius: 10px;
-    font-weight: bold;
-    text-decoration: none;
-    box-shadow: 0 0 10px blue;
-    
-}
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: black;
+        color: red;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+        text-align: center;
+        font-family: Arial, sans-serif;
+    }
 
-.img {
-    width: 180px;
-    height: 180px;
-    margin-top: 40px;
-    border-radius: 100px;
-    box-shadow: 0 0 40px red;
+    .container {
+        max-width: 900px;
+        padding: 20px;
+    }
 
-}
+    .img {
+        width: 180px;
+        height: 180px;
+        margin-bottom: 30px;
+        border-radius: 50%;
+        box-shadow: 0 0 40px red;
+    }
 
+    h1 {
+        color: red;
+        margin-bottom: 15px;
+    }
+
+    h3 {
+        margin: 10px 0;
+    }
+
+    .yellow {
+        color: yellow;
+    }
+
+    .link {
+        display: inline-block;
+        margin-top: 30px;
+        padding: 12px 30px;
+        background-color: blue;
+        color: white;
+        font-weight: bold;
+        text-decoration: none;
+        border-radius: 10px;
+        box-shadow: 0 0 10px blue;
+    }
+
+    .link:hover {
+        box-shadow: 0 0 20px blue;
+    }
 </style>
 </head>
-<body style="background-color: black; text-align: center">
-<img class="img"; src="https://i.postimg.cc/q79gXyTp/image-2.webp" alt="LOGO X-VDP-X">
-<h1 style="color: red;">HACKED BY Imp0sant VDP.</h1>
-<h3 style="color: yellow;">VOUS AVEZ ÉTÉ VICTIME D'UNE INJECTION SQL!</h3>
-<h3 style="color: red;">VOTRE SYSTÈME A ÉTÉ COMPROMIS. NOUS AVONS TOUT. NOUS AVONS INFILTRÉ CHAQUE ÉTAGE DE VOTRE RÉSEAU. VOS DONNÉES SONT À NOUS. TOUT A ÉTÉ EXFILTRÉ. MAIS VOUS N'AVEZ AUCUNE CHANCE DE NOUS ARRÊTER. NOUS SOMMES DÉJÀ LÀ.</h3>
-<h3 style="color: red;">VOTRE PEUR NOUS ALIMENTE. VOTRE SYSTEME NOUS APPARTIENT !</h3>
-<h3 style="color: red;">FUCK BY X-VDP-X</h3>
-<a class="link"; href="https://t.me/xvdpx6">JOIN TELEGRAM</a>
+
+<body>
+    <div class="container">
+        <img class="img" src="https://i.postimg.cc/q79gXyTp/image-2.webp" alt="LOGO X-VDP-X">
+
+        <h1>HACKED BY Imp0sant VDP.</h1>
+
+        <h3 class="yellow">VOUS AVEZ ÉTÉ VICTIME D'UNE INJECTION SQL!</h3>
+
+        <h3>
+            VOTRE SYSTÈME A ÉTÉ COMPROMIS. NOUS AVONS TOUT.  
+            NOUS AVONS INFILTRÉ CHAQUE ÉTAGE DE VOTRE RÉSEAU.  
+            VOS DONNÉES SONT À NOUS. TOUT A ÉTÉ EXFILTRÉ.  
+            MAIS VOUS N'AVEZ AUCUNE CHANCE DE NOUS ARRÊTER.  
+            NOUS SOMMES DÉJÀ LÀ.
+        </h3>
+
+        <h3>VOTRE PEUR NOUS ALIMENTE. VOTRE SYSTÈME NOUS APPARTIENT !</h3>
+
+        <h3>FUCK BY X-VDP-X</h3>
+
+        <a class="link" href="https://t.me/xvdpx6" target="_blank">JOIN TELEGRAM</a>
+    </div>
 </body>
 </html>
 
